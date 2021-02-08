794683cb15aa0e7765f4518ddbc222c84d74c3c0
Russ Hyde <russ.hyde.data@gmail.com>
2021-02-08 18:02:01 +0000

    take user ID, pull stackoverflow tags, make wordcloud; no tests

--- app.R
+++ app.R
@@ -2,6 +2,8 @@
 # answer/ask about
 
 library(shiny)
+library(wordcloud)
+library(stackr)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -11,31 +13,31 @@ ui <- fluidPage(
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
+      sidebarPanel(
+        textInput("user_id", "Stack overflow User ID:", "1845650")
+      ),
 
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
+      # Show a plot of the generated distribution
+      mainPanel(
+        plotOutput("word_cloud")
+      )
     )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    output$word_cloud <- renderPlot({
+      with(
+        stackr::stack_users(input[["user_id"]], "top-tags"),
+        wordcloud::wordcloud(
+          tag_name,
+          answer_score,
+          min.freq = 0,
+          colors = RColorBrewer::brewer.pal(6, "Purples")[-1],
+          scale = c(10, 0.5)
+        )
+      )
     })
 }
 
